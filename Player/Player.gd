52aa4be1ebf6b515a4566ad720ce11dbd5485265
connor mcfadden <0717330@schoolsnet.act.edu.au>
2022-02-28 10:29:57 +1030

    added rapid fire switch

--- Player/Player.gd
+++ Player/Player.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 var movement_speed = 750 
 var bulletSource = preload ("res://Bullet/Bullet.tscn")
-
+var rapidFire = false
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -15,12 +15,22 @@ func _ready():
 
 func _physics_process(delta):
 	if Input. is_action_pressed("ui_left"):
-		move_and_collide(Vector2(-movement_speed *delta, 0))
+		if position.x > 10:
+			move_and_collide(Vector2(-movement_speed * delta, 0))
 	if Input. is_action_pressed("ui_right"):
-		move_and_collide(Vector2(movement_speed * delta, 0))
-		
+		if position.x < 1250:
+			move_and_collide(Vector2(movement_speed * delta, 0))
+
 func _process(delta):
-	if Input.is_action_pressed("fire"):
-		var bulletInstance = bulletSource.instance()
-		bulletInstance.position = Vector2(position.x, position.y-20)
-		get_tree().get_root().add_child(bulletInstance)
+	if Input.is_action_just_pressed("ui_Ctrl"):
+		rapidFire = !rapidFire
+	if rapidFire:
+		if Input.is_action_pressed("fire"):
+			var bulletInstance = bulletSource.instance()
+			bulletInstance.position = Vector2(position.x, position.y-20)
+			get_tree().get_root().add_child(bulletInstance)
+	else:
+		if Input.is_action_just_pressed("fire"):
+			var bulletInstance = bulletSource.instance()
+			bulletInstance.position = Vector2(position.x, position.y-20)
+			get_tree().get_root().add_child(bulletInstance)
