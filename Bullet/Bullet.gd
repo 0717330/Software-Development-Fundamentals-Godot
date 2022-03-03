6a320b18408e04828d9b4fe4497d989787a8597b
connor mcfadden <0717330@schoolsnet.act.edu.au>
2022-03-03 11:41:11 +1030

    bang bang enemy dead now

--- Bullet/Bullet.gd
+++ Bullet/Bullet.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+
+
 var speed = 500
 
 # Declare member variables here. Examples:
@@ -8,6 +10,12 @@ func _ready():
 
 func _physics_process(delta):
 	var collidedObject = move_and_collide(Vector2(0, -speed*delta))
+	if (collidedObject):
+		print(collidedObject.collider.name)
+		if "Enemy" in collidedObject.collider.name:
+			collidedObject.get_collider().queue_free()
+			GlobalVariables.scoringInformation["currentScore"] +=10
+		queue_free()
 # Called when the node enters the scene tree for the first time.
 
 
--- Global.gd
+++ Global.gd
@@ -2,3 +2,10 @@ extends Node
 
 var bulletInstanceCount = 0 # Keeps track of how many bullet instances are current
 var enemyBulletInstanceCount = 0
+
+var scoringInformation = {
+	"currentScore": 0,
+	"currentPlayer": "user",
+	"highScore": 0,
+	"highScorePlayerName" : "winner"
+}
